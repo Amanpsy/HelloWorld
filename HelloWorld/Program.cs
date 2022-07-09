bfe1a58c4de81744fdf33f3c308eeb80d77ce460
code with aman <amankr97111@gmail.com>
2022-07-09 07:39:29 +0530

    initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("hey all hope all are good");
+Console.WriteLine("merge files with github");
